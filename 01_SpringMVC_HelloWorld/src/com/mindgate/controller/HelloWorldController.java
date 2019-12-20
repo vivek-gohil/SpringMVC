8bcce77f1daa5ba182d562dc0ef921e610e2a745
vivek-gohil <59043694+vivek-gohil@users.noreply.github.com>
2019-12-20 11:35:35 +0530

    Spring MVC :: 20th Dec 2019 :: HelloWorld Project

---
+++ 01_SpringMVC_HelloWorld/src/com/mindgate/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.mindgate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String show() {
+		return "welcome";
+	}
+}
